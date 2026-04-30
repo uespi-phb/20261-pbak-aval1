61330ffd985ea59add9e1dd78a3fb4f98b6182c3
Jordan Fernandes <fernandesjordan32@gmail.com>
2026-04-30 17:34:25 -0300

    test: implemented test 4.5 for team04 correction

--- test/test_name.spec.ts
+++ test/test_name.spec.ts
@@ -4,7 +4,7 @@ import { Name } from '#src/register-student/domain/name'
 
 describe('Teste unitário para o test 4.5', () => {
   test('should throw when name has less than 5 characters after trim', () => {
-    const input_value = 'Nome'
+    const input_value = ' Nome '
     const expected_value = 'Invalid name'
     const result = () => Name.create(input_value)
     expect(result).toThrow(expected_value)
