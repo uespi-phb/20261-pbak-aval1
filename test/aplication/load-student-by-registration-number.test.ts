d0059a9f81b07c680c125eada08bce1fda05ec5d
Felipe Martins <wendellmartins04@gmail.com>
2026-04-30 11:44:04 -0300

    test (LoadStudentByRegistrationNumber): implement assigned tests for should propagate error from LoadStudentByRegistrationNumber

---
+++ test/aplication/load-student-by-registration-number.test.ts
@@ -0,0 +1,45 @@
+//1.13
+import { describe, expect, test } from 'vitest'
+import type { MockProxy } from 'vitest-mock-extended'
+import { mock } from 'vitest-mock-extended'
+
+import type { GenerateId } from '#src/register-student/application/generate-id'
+import type { LoadStudentByEmail } from '#src/register-student/application/load-student-by-email'
+import type { LoadStudentByRegistrationNumber } from '#src/register-student/application/load-student-by-registration-number'
+import type { RegisterStudentInput } from '#src/register-student/application/register-student-dtos'
+import { RegisterStudentUseCase } from '#src/register-student/application/register-student-use-case'
+import type { SaveStudent } from '#src/register-student/application/save-student'
+
+describe('RegisterStudentUseCase', () => {
+  let loadStudentByEmail: MockProxy<LoadStudentByEmail>
+  let loadStudentByRegistrationNumber: MockProxy<LoadStudentByRegistrationNumber>
+  let saveStudent: MockProxy<SaveStudent>
+  let generateId: MockProxy<GenerateId>
+
+  beforeEach(() => {
+    loadStudentByEmail = mock<LoadStudentByEmail>()
+    loadStudentByRegistrationNumber = mock<LoadStudentByRegistrationNumber>()
+    loadStudentByRegistrationNumber.loadByRegistrationNumber.mockRejectedValue(
+      new Error('Error Load By Registration Number'),
+    )
+    saveStudent = mock<SaveStudent>()
+    generateId = mock<GenerateId>()
+  })
+
+  test('should propagate error from LoadStudentByRegistrationNumber', async () => {
+    const useCase = new RegisterStudentUseCase(
+      loadStudentByEmail,
+      loadStudentByRegistrationNumber,
+      saveStudent,
+      generateId,
+    )
+
+    const input: RegisterStudentInput = {
+      name: 'Eyder Rios',
+      email: 'eyderrios@email.com',
+      registrationNumber: '6931251',
+    }
+
+    await expect(useCase.execute(input)).rejects.toThrow(Error)
+  })
+})
